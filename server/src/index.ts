34892ca94a119bd78ecf8523455f0c0593603475
Sevethseal <kamura657@gmail.com>
2025-08-04 10:49:00 +0200

    Update server index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import router from "./routes";
@@ -21,11 +22,16 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// API routes
 app.use("/api", router);
 
-// 404 handler
-app.use((req: Request, res: Response) => {
-  res.status(404).json({ error: "Not Found" });
+// Serve React static files
+const buildPath = path.join(__dirname, "../../build");
+app.use(express.static(buildPath));
+
+// Catch-all to serve index.html for client-side routing
+app.get("*", (_req: Request, res: Response) => {
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
 // Error handler
